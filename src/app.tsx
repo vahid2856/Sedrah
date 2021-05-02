e627e74ee872cdc3279d0f1b0cd4186e21eca04b
Mostafa <mos.sameti@gmail.com>
2021-05-02 14:12:38 +0430

    add date-time picker libs

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,7 @@
 import { FC } from 'react';
+import jMoment from 'moment-jalaali';
+import JalaliUtils from '@date-io/jalaali';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import {
   ThemeProvider,
   StylesProvider,
@@ -9,14 +12,18 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { themeConfigs, rtlConfigs, Configs } from '@configs/index';
 import Tree from '@components/tree';
 
+jMoment.loadPersian({ dialect: 'persian-modern', usePersianDigits: true });
+
 const App: FC = () => {
   return (
     <ThemeProvider theme={createMuiTheme(themeConfigs)}>
       <CssBaseline />
       <StylesProvider jss={rtlConfigs}>
-        <Configs>
-          <Tree />
-        </Configs>
+        <MuiPickersUtilsProvider utils={JalaliUtils} locale="fa">
+          <Configs>
+            <Tree />
+          </Configs>
+        </MuiPickersUtilsProvider>
       </StylesProvider>
     </ThemeProvider>
   );
